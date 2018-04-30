4adc032f0da4ddab49738963b17853e9761f7fe2
Lebedev Artem <exces-st@yandex.ru>
2018-05-01 00:16:30 +0300

    add pageload

--- src/bin/pageloader.js
+++ src/bin/pageloader.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
-import ok from '..';
+import commander from 'commander';
+import pageLoad from '..';
+import { version } from '../../package.json';
 
-ok();
+
+const program = commander;
+
+program
+  .version(version)
+  .arguments('<url> [options] [path]')
+  .description('Download the page from web to specified folder(to the program\'s start directory by default )')
+  .option('--output', 'Output folder')
+  .action((url, option, path) => pageLoad(url, option, path))
+  .parse(process.argv);
+
+console.log('Page was downloaded');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export default () => console.log('All is OK');
+import axios from 'axios';
+import fs from 'mz/fs';
+import url from 'url';
+import path from 'path';
+
+
+const pathActions = {
+  undefined: path.resolve,
+  '--output': path.join,
+};
+
+const getAction = option => pathActions[option];
+
+const urlToFilename = (uri) => {
+  const regex = /\W/g;
+  const { protocol } = url.parse(uri);
+  const start = `${protocol}//`.length;
+  const fileName = uri.substr(start).replace(regex, '-');
+  return `${fileName}.html`;
+};
+
+const pageLoad = (uri, option, destPath = '') =>
+  axios.get(uri)
+    .then(({ data }) => {
+      const fileName = urlToFilename(uri);
+      const pathAction = getAction(option);
+      const fullPath = pathAction(destPath, fileName);
+      fs.writeFile(fullPath, data, 'utf8');
+    })
+    .catch(e => console.log(e));
+
+export default pageLoad;
