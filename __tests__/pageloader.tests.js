2d92e27e3bee5cd057e8eb47adc491d70328cd49
Lebedev Artem <exces-st@yandex.ru>
2018-05-03 02:16:33 +0300

    fix test

--- __tests__/pageloader.tests.js
+++ __tests__/pageloader.tests.js
@@ -2,38 +2,31 @@ import axios from 'axios';
 import nock from 'nock';
 import httpAdapter from 'axios/lib/adapters/http';
 import fs from 'mz/fs';
+import path from 'path';
+import os from 'os';
 import pageLoad from '../src';
 import half from '../src/half';
 
 
 const testData = '<html><head></head><body>test data</body></html>';
-const destPathTest = '__tests__/__fixtures__';
 
-const hostTest1 = 'http://test1.com';
-const urlTest1 = 'http://test1.com/test1';
-const pathToResultFileTest1 = '__tests__/__fixtures__/test1-com-test1.html';
-const pathToOutputDirTest1 = '__tests__/__fixtures__/test1-com-test1_files';
+const hostTest = 'http://test.com';
+const urlTest = 'http://test.com/test';
 
-const hostTest2 = 'http://test2.com';
-const urlTest2 = 'http://test2.com/test2';
-const pathToResultFileTest2 = '__tests__/__fixtures__/test2-com-test2.html';
-const pathToOutputDirTest2 = '__tests__/__fixtures__/test2-com-test2_files';
+const resultFileName = '/test-com-test.html';
+const outDirName = '/test-com-test_files';
+
+const pathToOutDirTest2 = '__tests__/__fixtures__';
 
 const urlTest3 = 'https://github.com/';
-const pathToResultFileTest3 = '__tests__/__fixtures__/github-com.html';
-const pathToOutputDirTest3 = '__tests__/__fixtures__/github-com_files';
-
+const resultFileNameTest3 = '/github-com.html';
+const outDirNameTest3 = '/github-com_files';
 
 axios.defaults.adapter = httpAdapter;
 
-nock(hostTest1)
+nock(hostTest)
   .persist()
-  .get('/test1')
-  .reply(200, testData);
-
-nock(hostTest2)
-  .persist()
-  .get('/test2')
+  .get('/test')
   .reply(200, testData);
 
 test('Check for correct jest work', () => {
@@ -41,30 +34,35 @@ test('Check for correct jest work', () => {
 });
 
 test('1. Test with --output. Output directory doesn\'t exist ', async () => {
-  await fs.writeFile(pathToResultFileTest1, '', 'utf8');
-  if (fs.existsSync(pathToOutputDirTest1)) {
-    await fs.rmdir(pathToOutputDirTest1);
-  }
-  await pageLoad(urlTest1, destPathTest);
-  const expectedData = await fs.readFile(pathToResultFileTest1, 'utf8');
+  const pathToOutDir = await fs.mkdtemp(`${os.tmpdir()}${path.sep}`);
+  const outDirPath = `${pathToOutDir}${outDirName}`;
+  const resultFilePath = `${pathToOutDir}${resultFileName}`;
+
+  await pageLoad(urlTest, pathToOutDir);
+  const expectedData = await fs.readFile(resultFilePath, 'utf8');
   expect(expectedData).toBe(testData);
+  expect(fs.existsSync(outDirPath)).toBeTruthy();
 });
 
 test('2. Test with --output. Output directory exists', async () => {
-  await fs.writeFile(pathToResultFileTest2, '', 'utf8');
-  if (fs.existsSync(pathToOutputDirTest2)) {
-    await fs.rmdir(pathToOutputDirTest2);
-  }
-  await pageLoad(urlTest2, destPathTest);
-  const expectedData = await fs.readFile(pathToResultFileTest2, 'utf8');
-  expect(fs.existsSync(pathToOutputDirTest2)).toBeTruthy();
+  const outDirPath = `${pathToOutDirTest2}${outDirName}`;
+  const resultFilePath = `${pathToOutDirTest2}${resultFileName}`;
+
+  await fs.writeFile(`${pathToOutDirTest2}${resultFileName}`, '', 'utf8');
+
+  await pageLoad(urlTest, pathToOutDirTest2);
+  const expectedData = await fs.readFile(resultFilePath, 'utf8');
+  expect(fs.existsSync(outDirPath)).toBeTruthy();
   expect(expectedData).toBe(testData);
 });
 
-test('4. Test with --output. Loaded page is external main page', async () => {
-  await fs.writeFile(pathToResultFileTest3, '', 'utf8');
-  await pageLoad(urlTest3, destPathTest);
-  expect(fs.existsSync(pathToOutputDirTest3)).toBeTruthy();
-  expect(fs.existsSync(pathToResultFileTest3)).toBeTruthy();
+test('3. Test with --output. Loaded page is external page', async () => {
+  const pathToOutDir = await fs.mkdtemp(`${os.tmpdir()}${path.sep}`);
+  const outDirPath = `${pathToOutDir}${resultFileNameTest3}`;
+  const resultFilePath = `${pathToOutDir}${outDirNameTest3}`;
+
+  await pageLoad(urlTest3, pathToOutDir);
+  expect(fs.existsSync(outDirPath)).toBeTruthy();
+  expect(fs.existsSync(resultFilePath)).toBeTruthy();
 });
 
