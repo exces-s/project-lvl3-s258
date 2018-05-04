89e981e146cd9629112c65fc39a8b6865aa12ff1
Lebedev Artem <exces-st@yandex.ru>
2018-05-04 23:32:01 +0300

    update makefile and version

--- Makefile
+++ Makefile
@@ -21,3 +21,6 @@ build:
 
 debug:
 	DEBUG=pa* npm test
+
+q:
+	DEBUG=pa* npm run babel-node -- src/bin/pageloader.js https://github.com/ --output __tests__/__fixtures__/
--- __tests__/pageloader.tests.js
+++ __tests__/pageloader.tests.js
@@ -7,6 +7,7 @@ import os from 'os';
 import pageLoad from '../src';
 import half from '../src/half';
 
+axios.defaults.adapter = httpAdapter;
 
 const testData = '<html><head></head><body>test data</body></html>';
 
@@ -19,60 +20,75 @@ const urlTest3 = 'https://github.com/';
 const resultFileNameTest3 = '/github-com.html';
 const outDirNameTest3 = '/github-com_files';
 
-axios.defaults.adapter = httpAdapter;
-
-nock(hostTest)
-  .persist()
+beforeEach(() => nock(hostTest)
   .get('/test')
-  .reply(200, testData);
+  .reply(200, testData));
+
+beforeEach(() => nock(hostTest)
+  .get('/404')
+  .reply(404, testData));
+
 
 test('Check for correct jest work', () => {
   expect(half(6)).toBe(3);
 });
 
-test('1. Test with --output. Output directory doesn\'t exist ', async () => {
+test('1. Option: --output. Output directory doesn\'t exist ', async () => {
   const pathToOutDir = await fs.mkdtemp(`${os.tmpdir()}${path.sep}`);
-
-  const outDirPath = `${pathToOutDir}/test-com-test_files`;
+  const outDirFullPath = `${pathToOutDir}/test-com-test_files`;
   const resultFilePath = `${pathToOutDir}/test-com-test.html`;
 
   await pageLoad(urlTest, pathToOutDir);
+
+  const isOutDirExist = fs.existsSync(outDirFullPath);
   const expectedData = await fs.readFile(resultFilePath, 'utf8');
+
   expect(expectedData).toBe(testData);
-  expect(fs.existsSync(outDirPath)).toBeTruthy();
+  expect(isOutDirExist).toBeTruthy();
 });
 
-test('2. Test with --output. Output directory exists', async () => {
-  const outDirPath = '__tests__/__fixtures__/test-com-test_files';
+test('2. Option: --output. Output directory exists', async () => {
+  const outDirFullPath = '__tests__/__fixtures__/test-com-test_files';
   const resultFilePath = '__tests__/__fixtures__/test-com-test.html';
+  const isFileExist = fs.existsSync(resultFilePath);
 
-  await fs.writeFile(resultFilePath, '', 'utf8');
-  // const bar = await fs.readFile(resultFilePath, 'utf8');
-  // console.log(bar);
-
+  if (isFileExist) {
+    await fs.unlink(resultFilePath);
+  }
   await pageLoad(urlTest, fixturesPath);
 
-  // const foo = await fs.readFile(resultFilePath, 'utf8');
-  // console.log(foo);
-
+  const isOutDirExist = fs.existsSync(outDirFullPath);
   const expectedData = await fs.readFile(resultFilePath, 'utf8');
-  expect(await fs.existsSync(outDirPath)).toBeTruthy();
-  // console.log(expectedData);
+
+  expect(isOutDirExist).toBeTruthy();
   expect(expectedData).toBe(testData);
 });
 
-test('3. Test with --output. Loaded page is external page', async () => {
+test('3. Option: --output. Loaded page is external page', async () => {
   const pathToOutDir = await fs.mkdtemp(`${os.tmpdir()}${path.sep}`);
   const resultFilePath = `${pathToOutDir}${resultFileNameTest3}`;
-  const outDirPath = `${pathToOutDir}${outDirNameTest3}`;
-
+  const outDirFullPath = `${pathToOutDir}${outDirNameTest3}`;
   const actualOpensearch = await fs.readFile(`${fixturesPath}/opensearch.xml`);
 
   await pageLoad(urlTest3, pathToOutDir);
 
-  const loadedOpensearch = await fs.readFile(`${outDirPath}/opensearch.xml`);
-  expect(fs.existsSync(outDirPath)).toBeTruthy();
-  expect(fs.existsSync(resultFilePath)).toBeTruthy();
+  const loadedOpensearch = await fs.readFile(`${outDirFullPath}/opensearch.xml`);
+  const isOutDirExist = fs.existsSync(outDirFullPath);
+  const isResultFileExist = fs.existsSync(resultFilePath);
+
+  expect(isOutDirExist).toBeTruthy();
+  expect(isResultFileExist).toBeTruthy();
   expect(loadedOpensearch).toEqual(actualOpensearch);
 });
 
+test('4. Option: --output. Incorrect output directory path', async () => {
+  const errMessage = 'NETWORK ERROR. Remote server error or network problems. Url: http://test.com/404';
+  const result = await pageLoad('http://test.com/404', `${fixturesPath}`);
+  expect(result).toEqual(errMessage);
+});
+
+test('5. Option: --output. Incorrect url', async () => {
+  const errMessage = 'ENOENT ERROR. No such file or directory. Check this path: /home/exces-s/Projects/page-loader/__tests__/__fixtures__/incorrectPath/test-com-test_files';
+  const result = await pageLoad('http://test.com/test', `${fixturesPath}/incorrectPath`);
+  expect(result).toEqual(errMessage);
+});
